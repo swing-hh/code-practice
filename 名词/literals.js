3c14818320482a944a0800bd12592cc654437a52
yaojintao <yaojintao@zuoyebang.com>
2021-03-14 13:15:55 +0800

    学习了js数据类型的一部分内容

---
+++ 名词/literals.js
@@ -0,0 +1,13 @@
+// 数字字面量
+console.log(30)// 十进制
+console.log(036); // 八进制
+console.log(0x1e); // 十六进制
+console.log(.122); // 浮点数整数为0可以写
+console.log(3.15e5); // e表示10的几次幂
+console.log(3e45645645645645); // 试图表示一个非常大的数字就会Infinity/-Infinity（正无穷或负无穷）
+
+// 字符串字面量
+console.log("今天天气很好"); // ""双引号
+console.log('今天天气很好'); // ''单引号
+console.log('\t今天天\n气很好'); // \t锁进 \n换行 
+console.log("c:\\a\\b.jpg"); // 有些不能直接使用的符号要使用\转译一下
