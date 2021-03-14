f646d0281db30da9f25267a080bc334f7cae3d6a
yaojintao <yaojintao@zuoyebang.com>
2021-03-14 13:09:39 +0800

    学习了js数据类型的一部分内容

---
+++ ECMAScript5/基本概念/index.js
@@ -0,0 +1,31 @@
+"use strict"
+var A = 200;
+a =  false
+
+var a = 100;
+function getName() {
+  var b = 10;
+}
+
+// var eval = 100; // 严格模式下报错
+console.log(a, A);
+
+console.log(typeof 1);
+console.log(typeof "1");
+console.log(typeof false);
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof []);
+console.log(typeof {});
+console.log(typeof function () {});
+
+var cc;
+console.log(cc);
+cc = 100;
+
+console.log(dd);
+var dd = 100;
+
+console.log(undefined == null);
+
+console.log(Boolean("message"))
